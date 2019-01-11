ef4626553fd3a4930f34c08fb9a38f45afa98149
Harshita Jain <harshitaj270@gmail.com>
2019-01-11 20:59:40 +0530

    first exercise solution

--- answer_exercise2.cs
+++ answer_exercise2.cs
@@ -6,8 +6,8 @@ namespace Rextester
     {
         public static void Main(string[] args)
         {
-            char[] array = { 'z','q','a','u','w','r','c','f' }; // Input array.
-        Array.Sort<char>(array);          // Call sort.
+            char[] array = { 'z','q','a','u','w','r','c','f' }; // Input array
+        Array.Sort<char>(array);          // Call sort
         foreach (var c in array)
             Console.WriteLine(c);
         }
